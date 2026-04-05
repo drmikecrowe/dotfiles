05690bb2a6dd1514d4852b0b20f53f1e33ddd597
Mike Crowe <drmikecrowe@gmail.com>
2026-04-05 18:01:29 -0400

    Update .config/dorothy/config/environment.bash

--- private_dot_config/dorothy/config/environment.bash
+++ private_dot_config/dorothy/config/environment.bash
@@ -3,7 +3,7 @@
 # Used by `setup-environment-commands`
 
 # To enable caching (10 minutes validity):
-__cache --validity-seconds=600 -- xxhsum || exit
+# __cache --validity-seconds=600 -- xxhsum || exit
 
 # Add your custom environment variable exports here:
 # export MY_VAR="value"
